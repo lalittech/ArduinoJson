db7938e6edf05e58fb4ae50b6621e64dc309a6d9
Benoit Blanchon <github@benoitblanchon.fr>
2018-05-25 16:02:03 +0200

    Test serializeMsgPack() return value

--- test/MsgPackSerializer/destination_types.cpp
+++ test/MsgPackSerializer/destination_types.cpp
@@ -10,33 +10,38 @@ TEST_CASE("serialize MsgPack to various destination types") {
   JsonObject &object = doc.to<JsonObject>();
   object["hello"] = "world";
   const char *expected_result = "\x81\xA5hello\xA5world";
+  const size_t expected_length = 13;
 
   SECTION("std::string") {
     std::string result;
-    serializeMsgPack(object, result);
+    size_t len = serializeMsgPack(object, result);
 
     REQUIRE(expected_result == result);
+    REQUIRE(expected_length == len);
   }
 
   /*  SECTION("std::vector<char>") {
       std::vector<char> result;
-      serializeMsgPack(object, result);
+      size_t len = serializeMsgPack(object, result);
 
       REQUIRE(std::vector<char>(expected_result, expected_result + 13) ==
     result);
+    REQUIRE(expected_length == len);
     } */
 
   SECTION("char[]") {
     char result[64];
-    serializeMsgPack(object, result);
+    size_t len = serializeMsgPack(object, result);
 
     REQUIRE(std::string(expected_result) == result);
+    REQUIRE(expected_length == len);
   }
 
   SECTION("char*") {
     char result[64];
-    serializeMsgPack(object, result, 64);
+    size_t len = serializeMsgPack(object, result, 64);
 
     REQUIRE(std::string(expected_result) == result);
+    REQUIRE(expected_length == len);
   }
 }
